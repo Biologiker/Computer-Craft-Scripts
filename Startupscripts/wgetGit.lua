90e86cb410db8cd8287741fe393c2a4ae544a672
Biologiker <quentink@gmx.de>
2024-12-15 03:46:47 +0100

    wgetGit auf Warehouse geupdated und getStartupScript wieder gelöscht

--- Startupscripts/wgetGit.lua
+++ Startupscripts/wgetGit.lua
@@ -1,1 +1,6 @@
-shell.run("wget https://raw.githubusercontent.com/Biologiker/Computer-Craft-Scripts/refs/heads/main/activateMobfarm.lua")
+local relativePath = "Startupscripts/warehouseV3.lua"
+local baseUrl = "https://raw.githubusercontent.com/Biologiker/Computer-Craft-Scripts/refs/heads/main/ "
+local finalFileName = "warehouse"
+
+shell.run(string.format("mv %s_old", finalFileName))
+shell.run(string.format("wget %s%s %s", baseUrl, relativePath, finalFileName))
--- getStartupScript.lua
+++
@@ -1,6 +0,0 @@
-local relativePath = "Startupscripts/wgetGit.lua"
-local baseUrl = "https://raw.githubusercontent.com/Biologiker/Computer-Craft-Scripts/refs/heads/main/ "
-local finalFileName = "startup"
-
-shell.run(string.format("mv %s_old", finalFileName))
-shell.run(string.format("wget %s%s %s", baseUrl, relativePath, finalFileName))
