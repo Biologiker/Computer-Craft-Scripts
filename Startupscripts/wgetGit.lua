3c4dc19f81166ad4e71b992b71e758b269aa4657
Biologiker <quentink@gmx.de>
2024-12-15 03:46:47 +0100

    wgetGit fix

--- Startupscripts/wgetGit.lua
+++ Startupscripts/wgetGit.lua
@@ -1,6 +1,7 @@
-local relativePath = "Startupscripts/warehouseV3.lua"
-local baseUrl = "https://raw.githubusercontent.com/Biologiker/Computer-Craft-Scripts/refs/heads/main/ "
+local relativePath = "warehouseV3.lua"
+local baseUrl = "https://raw.githubusercontent.com/Biologiker/Computer-Craft-Scripts/refs/heads/main/"
 local finalFileName = "warehouse"
 
-shell.run(string.format("mv %s_old", finalFileName))
-shell.run(string.format("wget %s%s %s", baseUrl, relativePath, finalFileName))
+shell.run(string.format("rm %s_old", finalFileName))
+shell.run(string.format("mv %s %s_old", finalFileName, finalFileName))
+shell.run(string.format("wget run %s%s %s", baseUrl, relativePath, finalFileName))
