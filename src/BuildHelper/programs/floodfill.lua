d9c6978586dce73ac1a2fdf73eb50ad7d82999ba
Biologiker <quentink@gmx.de>
2024-12-21 19:55:04 +0100

    now he should return after without turning 4 times

--- src/BuildHelper/programs/floodfill.lua
+++ src/BuildHelper/programs/floodfill.lua
@@ -42,7 +42,7 @@ function floodfill.Start(args)
                 table.insert(freeBlocksToRight, xIterator)
             end
 
-            if hasBlockInfront == false or xIterator < x then
+            if hasBlockInfront == false and xIterator < x then
                 turtle.turnLeft()
                 turtle.forward()
 
