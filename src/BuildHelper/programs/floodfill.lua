a68cda264a9a9ff07dcb17971cc32d1ee9282f28
Biologiker <quentink@gmx.de>
2024-12-21 21:59:36 +0100

    maybe i picked the wrong number

--- src/BuildHelper/programs/floodfill.lua
+++ src/BuildHelper/programs/floodfill.lua
@@ -53,7 +53,7 @@ function floodfill.Start(args)
                     hasBlockToRight = turtle.inspect()
 
                     print(textutils.serialize(freeBlocksToRight))
-                    print(tostring(xCoordinate - 2) .. ',')
+                    print(tostring(xCoordinate - 1) .. ',')
                     print (string.find(textutils.serialize(freeBlocksToRight), tostring(xCoordinate - 2) .. ','))
                     local wayBackIsFree = string.find(textutils.serialize(freeBlocksToRight), tostring(xCoordinate - 2) .. ',')
 
