8244b06291b66422c6674c8d57bfd7e9233cb606
Biologiker <quentink@gmx.de>
2024-12-21 18:33:01 +0100

    maybe it works

--- src/BuildHelper/programs/floodfill.lua
+++ src/BuildHelper/programs/floodfill.lua
@@ -5,13 +5,13 @@ function floodfill.Start(args)
     if #args < 2 then
         print('Please specify a Z coordinate relative to the turtle as a starting height!')
 
-        return;
+        return
     end
 
     if string.lower(args[2]) == 'help' or string.lower(args[2]) == 'info' then
         helpPages.FloodFillHelp()
 
-        return;
+        return
     end
 
     if #args < 4 then
@@ -42,29 +42,34 @@ function floodfill.Start(args)
                 table.insert(freeBlocksToRight, xIterator)
             end
 
-            if hasBlockInfront or xIterator == x then
-                return
+            if hasBlockInfront == false or xIterator < x then
+                turtle.turnLeft()
+                turtle.forward()
+
+                turtle.turnRight()
+                turtle.turnRight()
+
+                if hasBlockToRight and #freeBlocksToRight == 0 then
+                    table.insert(placedBlocks, { xIterator, yIterator })
+
+                    turtle.place()
+                end
+
+                turtle.turnRight()
+                turtle.turnRight()
+            else
+                turtle.turnRight()
+
+                xIterator = 0
+
+                goto continue
             end
-
-            turtle.turnLeft()
-            turtle.forward()
-
-            turtle.turnRight()
-            turtle.turnRight()
-
-            if hasBlockToRight and #freeBlocksToRight == 0 then
-                table.insert(placedBlocks, { xIterator, yIterator })
-
-                turtle.place()
-            end
-
-            turtle.turnRight()
-            turtle.turnRight()
         else
             break
         end
 
         xIterator = xIterator + 1
+        ::continue::
     end
 
     print('freeBlocksToRight')
