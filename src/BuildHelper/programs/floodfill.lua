bdf94cb47ecec211de4d84b3c82762aea101dfad
Biologiker <quentink@gmx.de>
2024-12-21 14:07:49 +0100

    fire try of placing blocks behind the turtle

--- src/BuildHelper/programs/floodfill.lua
+++ src/BuildHelper/programs/floodfill.lua
@@ -2,7 +2,7 @@ local floodfill = {}
 local helpPages = require('helpPages')
 
 function floodfill.Start(args)
-    if #args <= 2 then
+    if #args < 2 then
         print('Please specify a Z coordinate relative to the turtle as a starting height!')
 
         return;
@@ -14,7 +14,7 @@ function floodfill.Start(args)
         return;
     end
 
-    if #args <= 4 then
+    if #args < 4 then
         print('Please specify X and Y coordinates as a work region (0 would be "infinite")')
     end
 
@@ -22,18 +22,38 @@ function floodfill.Start(args)
     local x = args[3]
     local y = args[4]
 
+    local freeSides = {}
+
     for i = 1, x do
-        local has_block, data = turtle.inspect()
-        
-        if has_block then
-            print('Has block')
+        turtle.turnRight()
 
-            print(textutils.serialise(data))
-        else
-            print('Doesnt have Block')
+        local hasBlock, data = turtle.inspect()
 
-            print(textutils.serialise(data))
+        if hasBlock == false then
+            table.insert(freeSides, i)
         end
+
+        turtle.turnLeft()
+        turtle.forward()
+
+        turtle.turnRight()
+        turtle.turnRight()
+
+        if hasBlock and #freeSides == 0 then
+           turtle.place()
+        end
+
+        -- local has_block, data = turtle.inspect()
+        
+        -- if has_block then
+        --     print('Has block')
+
+        --     print(textutils.serialise(data))
+        -- else
+        --     print('Doesnt have Block')
+
+        --     print(textutils.serialise(data))
+        -- end
 
         -- turtle.placeDown()
     end
